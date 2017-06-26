85780f30d83ac2c834ede6bd1fe3546d1ce481cc
Mitaju <dmitry.filimonov@moex.com>
2017-06-26 17:10:38 +0300

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -1,44 +1,34 @@
-import json
+import yaml
+import pprint
 
 
-def get_cook_book(filename):
-    with open(filename) as fp:
-        cook_book = json.load(fp)
-        return cook_book
+def cook_book_input():
+    with open("menu_recepture.yml") as func:
+        cook_book = yaml.load(func)
+    return cook_book
 
 
 def get_shop_list_by_dishes(dishes, person_count):
-    cook_book = get_cook_book('dishes.json')
     shop_list = {}
+    cook_book = cook_book_input()
     for dish in dishes:
-        if dish not in cook_book:
-            print('Блюдо', dish, 'отсутствует в книге рецептов', '\n')
-            continue
-        for ingredient in cook_book[dish]:
-            new_shop_list_item = dict(ingredient)
-
+        for new_shop_list_item in cook_book[dish]:
             new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingredient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
+            if new_shop_list_item['ingridient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
             else:
-                shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
-
-    return shop_list
-
-
-def print_shop_list(shop_list):
-    for shop_list_item in shop_list.values():
-        print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'],
-                                shop_list_item['measure']))
+                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += \
+                new_shop_list_item['quantity']
+    return list(shop_list.values())
 
 
 def create_shop_list():
     person_count = int(input('Введите количество человек: '))
-    dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
-        .lower().split(',')
-    dishes = list(map(str.strip, dishes))
+    dishes = input('Введите блюда в расчете на одного человека (через запятую): '). \
+        split(', ')
     shop_list = get_shop_list_by_dishes(dishes, person_count)
-    print_shop_list(shop_list)
-
-
+    pprint.pprint(shop_list)
+    with open('result.yml', 'w') as f:
+        yaml.dump(shop_list, f, allow_unicode=True)
+        
 create_shop_list()
